5a34cd724d077cfde05d86c3ce1b57aa1981b3d5
David Duefrene <fortismilite@gmail.com>
2022-06-20 11:51:02 -0600

    Auth flow now gets refresh token

---
+++ index.ts
@@ -0,0 +1,100 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+require('dotenv').config();
+const { ApolloServer, gql } = require('apollo-server');
+const { app, BrowserWindow, shell, ipcMain } = require('electron');
+const { fork } = require('node:child_process');
+
+// GraphQL code
+const typeDefs = gql`
+    type Book {
+        title: String
+        author: String
+    }
+
+    type Query {
+        books: [Book]
+    }
+`;
+
+// Example data
+const books = [
+	{
+		title: 'The Awakening',
+		author: 'Kate Chopin',
+	},
+	{
+		title: 'City of Glass',
+		author: 'Paul Auster',
+	},
+];
+
+// Resolvers define the technique for fetching the types defined in the
+// schema. This resolver retrieves books from the "books" array above.
+const resolvers = {
+	Query: {
+		books: () => books,
+	},
+};
+
+// The ApolloServer constructor requires two parameters: your schema
+// definition and your set of resolvers.
+const server = new ApolloServer({
+	typeDefs,
+	resolvers,
+	csrfPrevention: true,
+});
+
+// The `listen` method launches a web server.
+// TODO do something better than console log
+server.listen().then(({ url }) => {
+	console.log(`🚀  Server ready at ${url}`);
+});
+
+// Electron code
+const createWindow = () => {
+	const win = new BrowserWindow({
+		width: 800,
+		height: 600,
+		webPreferences: {
+			nodeIntegration: true,
+			contextIsolation: false,
+			nodeIntegrationInWorker: true,
+			// preload: path.join(__dirname, 'preload.js'),
+		},
+	});
+
+	ipcMain.on('Login', (event) => {
+		event.preventDefault();
+		const controller = new AbortController();
+		const { signal } = controller;
+		const child = fork('./Electron/Auth/auth.ts', { signal });
+		child.on('message', (message) => {
+			shell.openExternal(message.url);
+		});
+		// child.on('error', (err) => {
+		// 	console.log(err);
+		// });
+		// controller.abort(); // Stops the child process
+	});
+
+	// TODO process.env.NODE_ENV seems to not be set dev despite it needing to be set to dev
+	// Check if we are in development mode
+	const isDev = process.env.NODE_ENV === 'dev' ||
+    process.env.NODE_ENV === 'development';
+	// building with vite appears to show up as undefined instead of production
+	process.env.NODE_ENV === undefined;
+	// eslint-disable-next-line no-constant-condition
+	true ?
+		win.loadURL('http://localhost:3000') :
+		win.loadFile('vite-build/index.html');
+};
+
+app.whenReady().then(() => {
+	createWindow();
+});
+
+app.on('window-all-closed', () => {
+	if (process.platform !== 'darwin') {
+		app.quit();
+	}
+});
--- src/App.tsx
+++ src/App.tsx
@@ -1,47 +1,47 @@
-import { useState } from 'react';
+import React, { useState } from 'react';
 
 import {
-  ApolloClient,
-  InMemoryCache,
-  ApolloProvider,
-  useQuery,
-  gql
-} from "@apollo/client";
+	ApolloClient,
+	InMemoryCache,
+	ApolloProvider,
+	useQuery,
+	gql
+} from '@apollo/client';
 
 import AddCharacter from './AddCharacter/AddCharacter';
 import logo from './logo.svg';
 import './App.css';
 
-  // the client
-  const client = new ApolloClient({
-    uri: 'http://localhost:4000',
-    cache: new InMemoryCache()
-  });
+// the client
+const client = new ApolloClient({
+	uri: 'http://localhost:4000',
+	cache: new InMemoryCache()
+});
 
 function App() {
-  const [count, setCount] = useState([]);
+	const [count, setCount] = useState([]);
 
-  client.query({
-    query: gql`
+	client.query({
+		query: gql`
     query ExampleQuery {
       books {
         title
       }
     }
     `
-  }).then(result => setCount(result.data.books));
+	}).then(result => setCount(result.data.books));
 
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite, React, GraphQL, and Electron!</p>
-        <p>env is {process.env.NODE_ENV}</p>
-        <AddCharacter />
-        <p>QL Query: {count.map(book => <p>{book['title']}</p>)}</p>
-      </header>
-    </div>
-  )
+	return (
+		<div className="App">
+			<header className="App-header">
+				<img src={logo} className="App-logo" alt="logo" />
+				<p>Hello Vite, React, GraphQL, and Electron!</p>
+				<p>env is {process.env.NODE_ENV}</p>
+				<AddCharacter />
+				<p>QL Query: {count.map(book => <p>{book['title']}</p>)}</p>
+			</header>
+		</div>
+	);
 }
 
-export default App
+export default App;
