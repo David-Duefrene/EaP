ef416ac318c166ec2f94c1c6af6c66f926244137
David Duefrene <1645298+David-Duefrene@users.noreply.github.com>
2022-09-18 21:31:26 -0600

    Added agent research to prisma

--- APICrawler/Endpoints/Character/agentResearch.ts
+++ APICrawler/Endpoints/Character/agentResearch.ts
@@ -1,14 +1,33 @@
+import prisma from '../../../prisma/PrismaClient'
 import ESIRequest from '../../axiosRequests/ESIRequest'
 import CharacterAuthData from '../../../Types/APIResponses/EveOfficial/axiosTypes/characterAuthData.type'
+import AgentResearch from '../../../Types/APIResponses/EveOfficial/AgentResearch.types'
 
 export default (characterAuthData: CharacterAuthData) => {
 	const { characterID, accessToken } = characterAuthData
 
 	return ESIRequest(`characters/${characterID}/agents_research`, accessToken)
-		.then((result) => {
-			// eslint-disable-next-line no-console
-			console.log('result', result)
-		}).catch((error) => {
+		.then((result: { data: Array<AgentResearch> }) => {
+			result.data.forEach((researchSlot) => {
+				const {
+					agent_id: agentID, points_per_day: pointsPerDay, remainder_points: remainderPoints,
+					skill_type_id: skillTypeID, started_at: startedAT,
+				} = researchSlot
+				const agentData = {
+					agentID, pointsPerDay, remainderPoints, skillTypeID, startedAT,
+				}
+
+				return prisma.AgentResearch.upsert({
+					where: { characterID },
+					update: {
+						data: { ...agentData },
+					},
+					create: {
+						data: { ...agentData },
+					},
+				})
+			})
+		}).catch((error: Error) => {
 			// eslint-disable-next-line no-console
 			console.log('Agent research error\n', error)
 		})
---
+++ Types/APIResponses/EveOfficial/AgentResearch.types.ts
@@ -0,0 +1,11 @@
+type AgentResearch = {
+	characterID: string,
+	name: string,
+	agent_id: number,
+	points_per_day: number,
+	remainder_points: number,
+	skill_type_id: number,
+	started_at: string,
+}
+
+export default AgentResearch
