c5f11802ba0b5281afd06f37dd622031ef456efe
David Duefrene <1645298+David-Duefrene@users.noreply.github.com>
2022-09-28 20:09:51 -0600

    Added Blueprint endpoint

---
+++ APICrawler/Endpoints/Character/Blueprints.ts
@@ -0,0 +1,36 @@
+import prisma from '../../../prisma/PrismaClient'
+import ESIRequest from '../../axiosRequests/ESIRequest'
+
+import CharacterAuthData from '../../../Types/APIResponses/EveOfficial/axiosTypes/characterAuthData.type'
+import Blueprint from '../../../Types/APIResponses/EveOfficial/Blueprints.types'
+
+export default (characterAuthData: CharacterAuthData) => {
+	const { characterID, accessToken } = characterAuthData
+
+	return ESIRequest(`characters/${characterID}/blueprints`, accessToken)
+		.then((result: { data: Array<Blueprint> }) => {
+			result.data.forEach((blueprint) => {
+				const {
+					item_id: itemID, location_flag: locationFlag, location_id: locationID,
+					material_efficiency: materialEfficiency, quantity, runs, time_efficiency: timeEfficiency,
+					type_id: typeID,
+				} = blueprint
+				const blueprintData = {
+					itemID, locationFlag, locationID, materialEfficiency, quantity, runs, timeEfficiency, typeID,
+				}
+
+				return prisma.Blueprint.upsert({
+					where: { itemID },
+					update: {
+						data: { ...blueprintData },
+					},
+					create: {
+						data: { ...blueprintData },
+					},
+				})
+			})
+		}).catch((error: Error) => {
+			// eslint-disable-next-line no-console
+			console.log('Blueprint error\n', error)
+		})
+}
