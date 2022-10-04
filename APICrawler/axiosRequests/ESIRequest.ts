08222d502d18bfb35d04ece68620babd2cf57fb6
David Duefrene <1645298+David-Duefrene@users.noreply.github.com>
2022-10-04 10:11:19 -0600

    ESIRequest now will auto convert all object keys to camel case. This is to make it easier to work with the data in the front end.

--- APICrawler/axiosRequests/ESIRequest.ts
+++ APICrawler/axiosRequests/ESIRequest.ts
@@ -1,11 +1,38 @@
 const axios = require('axios')
 
 export default (path: string, accessToken = '', server='tranquility') => {
-	const config = {
+	// Create an axios.create object that converts the response data to camelCase
+	const esiAxios = axios.create({
+		baseURL: `https://esi.evetech.net/latest/${path}`,
 		headers: {
-			'Authorization': `Bearer ${accessToken}`,
+			Authorization: `Bearer ${accessToken}`,
 		},
+	})
+
+	const convertKeys = (obj: any) => {
+		const newData = {}
+
+		Object.keys(obj).forEach((key) => {
+			newData[key.replace(/([-_][a-z])/ig, (letter) => {
+				return letter.toUpperCase().replace('_', '')
+			})] = obj[key]
+		})
+		return newData
 	}
 
-	return axios.get(`https://esi.evetech.net/latest/${path}/?datasource=${server}`, config)
+	esiAxios.interceptors.response.use((response) => {
+		if (Array.isArray(response.data)) {
+			response.data = response.data.map((item: any) => {
+				return convertKeys(item)
+			})
+		} else if (typeof response.data === 'object' && !Array.isArray(response.data) && response.data !== null) {
+			response.data = convertKeys(response.data)
+		}
+		return response
+	}, (error: Error) => {
+		console.log(error)
+		return Promise.reject(error)
+	})
+
+	return esiAxios.get(`?datasource=${server}`)
 }
