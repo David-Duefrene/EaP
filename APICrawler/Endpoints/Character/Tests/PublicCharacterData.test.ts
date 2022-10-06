ecc1989c2e170a826431d8190fe1b980aa303d82
David Duefrene <1645298+David-Duefrene@users.noreply.github.com>
2022-10-05 20:52:41 -0600

    ...

---
+++ APICrawler/Endpoints/Character/Tests/PublicCharacterData.test.ts
@@ -0,0 +1,40 @@
+import {
+	expect, test, afterEach, describe, vi,
+} from 'vitest'
+
+// comment
+import publicCharacterData from '../PublicCharacterData'
+import ESIRequest from '../../../axiosRequests/ESIRequest'
+
+describe('publicCharacterData', () => {
+	afterEach(() => {
+		vi.restoreAllMocks()
+	})
+
+	test('should be able to get public character data', async () => {
+		vi.mock('../../../axiosRequests/ESIRequest', async () => ({
+			default: vi.fn().mockResolvedValue({
+				data: {
+					name: 'Test Name',
+					corporation_id: 1,
+					alliance_id: 1,
+					security_status: 1,
+					birthday: new Date,
+					bloodline_id: 1,
+					description: 'description',
+					gender: 'male',
+					race_id: 1,
+				},
+			}),
+		}))
+
+		vi.mock('../../../../prisma/PrismaClient', () => ({
+			default: { Character: { upsert: vi.fn().mockResolvedValue({ }) } },
+		}))
+
+		publicCharacterData({ characterID: '1', accessToken: 'Token' })
+
+		expect(ESIRequest).toBeCalledTimes(1)
+		expect(ESIRequest).toBeCalledWith('characters/1')
+	})
+})
