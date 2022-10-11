b6ce39742f298c9601ac86e4390992d87a75e70a
David Duefrene <1645298+David-Duefrene@users.noreply.github.com>
2022-10-11 17:23:23 -0600

    fixed PublicCharacterSheet test

--- APICrawler/Endpoints/Character/Tests/PublicCharacterData.test.ts
+++ APICrawler/Endpoints/Character/Tests/PublicCharacterData.test.ts
@@ -4,6 +4,7 @@ import {
 
 import PublicCharacterData from '../PublicCharacterSheet'
 import ESIRequest from '../../../axiosRequests/ESIRequest'
+import prisma from '../../../../prisma/PrismaClient'
 
 describe('PublicCharacterData', () => {
 	afterEach(() => {
@@ -11,29 +12,65 @@ describe('PublicCharacterData', () => {
 	})
 
 	test('should be able to get public character data', async () => {
-		vi.mock('../../../axiosRequests/ESIRequest', async () => ({
+		vi.mock('../../../axiosRequests/ESIRequest', () => ({
 			default: vi.fn().mockResolvedValue({
 				data: {
-					name: 'Test Name',
-					corporation_id: 1,
-					alliance_id: 1,
-					security_status: 1,
-					birthday: new Date,
-					bloodline_id: 1,
+					name: 'name',
+					characterID: 1,
+					ancestryID: 1,
+					birthday: new Date('2022-10-06T02:09:38.981Z'),
+					bloodlineID: 1,
+					corporationID: 1,
 					description: 'description',
-					gender: 'male',
-					race_id: 1,
 				},
 			}),
 		}))
 
 		vi.mock('../../../../prisma/PrismaClient', () => ({
-			default: { Character: { upsert: vi.fn().mockResolvedValue({ }) } },
+			default: { Character: { upsert: vi.fn().mockResolvedValue(null) } },
 		}))
 
-		PublicCharacterData({ characterID: '1', accessToken: 'Token' })
-
+		await PublicCharacterData({ characterID: '1', accessToken: 'Token' })
 		expect(ESIRequest).toBeCalledTimes(1)
 		expect(ESIRequest).toBeCalledWith('characters/1')
+		expect(prisma.Character.upsert).toBeCalledTimes(1)
+		const mockData = {
+			where: { characterID: '1' },
+			create: {
+				characterID: '1',
+				characterSheet: {
+					create: {
+						name: 'name',
+						characterID: 1,
+						ancestryID: 1,
+						birthday: new Date('2022-10-06T02:09:38.981Z'),
+						bloodlineID: 1,
+						corporationID: 1,
+						description: 'description',
+					},
+				},
+				name: 'name',
+			},
+			update: {
+				characterID: '1',
+				characterSheet: {
+					update: {
+						where: { name: 'name' },
+						data: {
+							name: 'name',
+							characterID: 1,
+							ancestryID: 1,
+							birthday: new Date('2022-10-06T02:09:38.981Z'),
+							bloodlineID: 1,
+							corporationID: 1,
+							description: 'description',
+						}
+					},
+				},
+				name: 'name',
+			},
+		}
+
+		expect(prisma.Character.upsert).toBeCalledWith(mockData)
 	})
 })
