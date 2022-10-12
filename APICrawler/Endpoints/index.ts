a3ccffe55d1f5cffe9963d5a3218fc34fd573703
David Duefrene <1645298+David-Duefrene@users.noreply.github.com>
2022-10-12 16:36:10 -0600

    Notifications now save to DB

--- APICrawler/Endpoints/index.ts
+++ APICrawler/Endpoints/index.ts
@@ -4,7 +4,8 @@ import blueprint from './Character/Blueprints'
 import corpHistory from './Character/CorpHistory'
 import fatigue from './Character/Fatigue'
 import medals from './Character/Medals'
+import notifications from './Character/Notifications'
 
 export default [
-	publicCharacterData, agentResearch, blueprint, corpHistory, fatigue, medals,
+	publicCharacterData, agentResearch, blueprint, corpHistory, fatigue, medals, notifications,
 ]
