cd9882cec689a1f2ebb88f0b43c4ae0adb2442f9
David Duefrene <1645298+David-Duefrene@users.noreply.github.com>
2022-10-19 14:39:56 -0600

    ?

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,9 @@
 module.exports = {
-<<<<<<< HEAD
-	'env': {
-		'browser': true,
-		'es2021': true,
-	},
-	'extends': [
-=======
 	env: {
 		browser: true,
 		es2021: true,
 	},
 	extends: [
->>>>>>> 109b5e4b39793ce40c2324ac7f40f0e12bd70d6f
 		'eslint-config-vigilant-octo-train',
 		'eslint:recommended',
 		'plugin:react/recommended',
