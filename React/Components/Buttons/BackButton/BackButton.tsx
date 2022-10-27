d1d3d7e05c55938d84cff1789e478650ae1c5b28
David Duefrene <1645298+David-Duefrene@users.noreply.github.com>
2022-10-27 09:35:50 -0600

    Made back button use css modules

--- React/Components/Buttons/BackButton/BackButton.tsx
+++ React/Components/Buttons/BackButton/BackButton.tsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import { useNavigate } from 'react-router-dom'
 
-import './BackButton.css'
+import CSS from './BackButton.module.css'
 
 const BackButton = () => {
 	const navigate = useNavigate()
 
 	return (
-		<a onClick={() => navigate(-1)}>Back</a>
+		<a className={CSS.Button} onClick={() => navigate(-1)}>Back</a>
 	)
 }
 
