2518b810528f03cef9fa6ce11a3770a715c02e6b
David Duefrene <1645298+David-Duefrene@users.noreply.github.com>
2022-11-01 11:52:03 -0600

    add character

--- React/Components/Buttons/AddCharacter/AddCharacter.tsx
+++ React/Components/Buttons/AddCharacter/AddCharacter.tsx
@@ -2,6 +2,7 @@
 const { ipcRenderer } = require('electron')
 
 import React from 'react'
+import LoginWhite from './eve-sso-login-white-large.png'
 
 /**
  * A component that allows the user to log a character in and add it to the database
@@ -9,7 +10,7 @@ import React from 'react'
  * @returns {JSX.Element}
  */
 const AddCharacter = () => {
-	return <button type='button' onClick={() => ipcRenderer.send('Login')}>Login</button>
+	return <img src={LoginWhite} alt='Add new character' onClick={() => ipcRenderer.send('Login')} />
 }
 
 export default AddCharacter
