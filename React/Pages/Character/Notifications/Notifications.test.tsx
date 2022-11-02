844ab107325bfc06adf3f0e2faa29ffb48b09f75
David Duefrene <1645298+David-Duefrene@users.noreply.github.com>
2022-11-02 17:50:15 -0600

    155 notifications (#156)
    * Notifications
    * test

---
+++ React/Pages/Character/Notifications/Notifications.test.tsx
@@ -0,0 +1,70 @@
+// @vitest-environment jsdom
+import React from 'react'
+import {
+	createBrowserRouter, createRoutesFromElements, RouterProvider, Route,
+} from 'react-router-dom'
+
+import {
+	expect, test, afterEach, describe, beforeEach, vi,
+} from 'vitest'
+
+import {
+	cleanup, render, screen, waitForElementToBeRemoved,
+} from '@testing-library/react'
+
+import Notifications from './Notifications'
+
+describe('Notifications', () => {
+	beforeEach(() => {
+		vi.mock('../../../../prisma/PrismaClient', () => ({
+			default: {
+				Notification: {
+					findMany: vi.fn().mockResolvedValue([
+						{
+							notificationID: 1,
+							notificationCharacterID: 1,
+							notificationType: 'Character',
+							notificationText: 'Notification 1',
+							notificationSentDate: new Date(),
+							notificationRead: false,
+						},
+						{
+							notificationID: 2,
+							notificationCharacterID: 1,
+							notificationType: 'Character',
+							notificationText: 'Notification 2',
+							notificationSentDate: new Date(),
+							notificationRead: false,
+						},
+					]),
+				},
+			},
+		}))
+
+		const router = createBrowserRouter(createRoutesFromElements(
+			<Route path='/' element={<Notifications />} />,
+		))
+
+		render(<RouterProvider router={router} />)
+	})
+
+	afterEach(() => {
+		cleanup()
+	})
+
+	test('loads loading... on initial state', () => {
+		expect(screen).toBeTruthy()
+		expect(screen.getByText('Loading...')).toBeTruthy()
+	})
+
+	test('renders a back button', async () => {
+		await waitForElementToBeRemoved(() => screen.getByText('Loading...'))
+		const button = await screen.findByText('Back')
+		expect(button.tabIndex).toEqual(0)
+	})
+
+	test('renders a table', async () => {
+		await waitForElementToBeRemoved(() => screen.getByText('Loading...'))
+		expect(screen.findAllByRole('table')).toBeTruthy()
+	})
+})
