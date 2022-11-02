4e549338662261a5259c9e68880667c15e34a83d
David Duefrene <1645298+David-Duefrene@users.noreply.github.com>
2022-11-01 22:14:03 -0600

    fixed contact notification test

--- React/Pages/Character/ContactNotifications/ContactNotifications.test.tsx
+++ React/Pages/Character/ContactNotifications/ContactNotifications.test.tsx
@@ -1,42 +1,55 @@
 // @vitest-environment jsdom
-/*
- *Import React from 'react'
- *import {
- *	createBrowserRouter, createRoutesFromElements, RouterProvider, Route,
- *} from 'react-router-dom'
- *
- *import {
- *	render, screen, waitForElementToBeRemoved,
- *} from '@testing-library/react'
- *
- *import ContactNotifications from './ContactNotifications'
- */
+import React from 'react'
 import {
-	expect, test, describe,
+	createBrowserRouter, createRoutesFromElements, RouterProvider, Route,
+} from 'react-router-dom'
+
+import {
+	cleanup, render, screen, waitForElementToBeRemoved,
+} from '@testing-library/react'
+
+import ContactNotifications from './ContactNotifications'
+
+import {
+	expect, test, describe, vi, beforeEach, afterEach,
 } from 'vitest'
 
 describe('ContactNotifications', () => {
-	/*
-	 * Test('should render a back button', async () => {
-	 * const router = createBrowserRouter(createRoutesFromElements(
-	 * <Route path='/' element={<ContactNotifications />} />,
-	 * ))
-	 * render(<RouterProvider router={router}></RouterProvider>)
-	 * await waitForElementToBeRemoved(() => screen.getByText('Loading...'))
-	 * const button = await screen.findByText('Back')
-	 * expect(button.tabIndex).toEqual(0)
-	 * })
-	 *
-	 * test('should render a Table', () => {
-	 * const router = createBrowserRouter(createRoutesFromElements(
-	 * <Route path='/' element={<ContactNotifications />} />,
-	 * ))
-	 * render(<RouterProvider router={router}></RouterProvider>)
-	 * const table = screen.findAllByRole('table')
-	 * expect(table).toBeTruthy()
-	 * })
-	 */
-	test('delay', () => {
-		expect(true).toBeTruthy()
+	beforeEach(() => {
+		vi.mock('../../../../prisma/PrismaClient', () => ({
+			default: {
+				contactNotification: {
+					findMany: vi.fn().mockResolvedValue([
+						{
+							message: 'messageDataText',
+							standingLevel: 'messageTypeText',
+							sentDate: 'sentDateText',
+							senderCharacterID: 4,
+							notificationID: 5,
+						},
+					]),
+				},
+			},
+		}))
+
+		afterEach(() => {
+			cleanup()
+		})
+
+		const router = createBrowserRouter(createRoutesFromElements(
+			<Route path='/' element={<ContactNotifications />} />,
+		))
+		render(<RouterProvider router={router}></RouterProvider>)
+	})
+
+	test('should render a back button', async () => {
+		await waitForElementToBeRemoved(() => screen.getByText('Loading...'))
+		const button = await screen.findByText('Back')
+		expect(button.tabIndex).toEqual(0)
+	})
+
+	test('should render a Table', () => {
+		const table = screen.findAllByRole('table')
+		expect(table).toBeTruthy()
 	})
 })
