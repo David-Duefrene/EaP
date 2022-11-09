bc68a152611fbfb15e81c457e96890dce3059927
David Duefrene <1645298+David-Duefrene@users.noreply.github.com>
2022-11-08 22:58:29 -0700

    removed unused variable

--- React/Components/Buttons/BackButton/BackButton.tsx
+++ React/Components/Buttons/BackButton/BackButton.tsx
@@ -2,7 +2,6 @@ import React from 'react'
 import { useNavigate } from 'react-router-dom'
 
 import Nav from '../Nav/Nav'
-import CSS from '../Nav/Nav.module.css'
 
 const BackButton = () => {
 	const navigate = useNavigate()
