0968ce614c3d3c2bcd41e1700f9bb12118b5ad17
David Duefrene <1645298+David-Duefrene@users.noreply.github.com>
2022-11-11 08:41:30 -0700

    172 look up the buttons in sortable list from a language json file (#174)
    * Character Card converted
    
    * u
    
    * Done
    
    * fixed tests

--- React/Components/CharacterCard/CharacterCard.tsx
+++ React/Components/CharacterCard/CharacterCard.tsx
@@ -1,7 +1,9 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+
 import CharacterQuery from '../../../Types/APIResponses/PrismaQueries/Character/CharacterSheetQueries.type'
 import './CharacterCard.css'
+
 const CharacterCard = (props: { character: CharacterQuery }) => {
 	const {
 		characterID, name, allianceID, birthday, bloodlineID, corporationID, gender, raceID, securityStatus,
@@ -11,15 +13,15 @@ const CharacterCard = (props: { character: CharacterQuery }) => {
 		<Link key={characterID} className='Card' to={`character/${characterID}`}>
 			<img alt={`${name}'s character portrait`} className='Portrait' src={`https://images.evetech.net/characters/${characterID}/portrait?tenant=tranquility&size=256`} />
 			<ul className='CardBody'>
-				<li>{name}</li>
-				<li>Character ID: {characterID}</li>
-				<li>Alliance ID: {allianceID}</li>
-				<li>Character birthday: {`${birthday}`}</li>
-				<li>Bloodline ID: {bloodlineID}</li>
-				<li>Corp ID: {corporationID}</li>
-				<li>Gender: {gender}</li>
-				<li>Race ID: {raceID}</li>
-				<li>Security Status: {securityStatus}</li>
+				<li>{`Name: ${name}`}</li>
+				<li>{`Character ID: ${characterID}`}</li>
+				<li>{`Alliance: ${allianceID}`}</li>
+				<li>{`Birthday: ${birthday}`}</li>
+				<li>{`Bloodline: ${bloodlineID}`}</li>
+				<li>{`Corporation: ${corporationID}`}</li>
+				<li>{`Gender: ${gender}`}</li>
+				<li>{`Race: ${raceID}`}</li>
+				<li>{`Security Status: ${securityStatus}`}</li>
 			</ul>
 		</Link>
 	)
