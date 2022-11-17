0fd2e21b1af6ab769b07e81bf0f206f7ec4d3520
David Duefrene <1645298+David-Duefrene@users.noreply.github.com>
2022-11-17 09:32:25 -0700

    Notification HTML & CSS

--- React/Components/Buttons/Notification/Notification.tsx
+++ React/Components/Buttons/Notification/Notification.tsx
@@ -1,10 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import CSS from './Notification.module.css'
 
 const Notification = () => {
+	const [ show, setShow ] = useState(false)
+	const [ messages, setMessages ] = useState([])
+
 	return (
-		<button className={CSS.Notification} type='button'>!</button>
+		<>
+			<button onClick={() => setShow(!show)} className={CSS.NotificationButton} type='button'>!</button>
+			<div className={`${CSS.NotificationArea} ${show ? CSS.Active : null}`}>
+				<ol className={CSS.NotificationList}>
+					{
+						messages.length > 0 ?
+							messages.map((message, index) => {
+								return (
+									<li className={CSS.NotificationItem} key={index}>{message}</li>
+								)
+							}) :
+							<li className={CSS.NotificationItem}>No messages</li>
+					}
+				</ol>
+			</div>
+		</>
 	)
 }
 
