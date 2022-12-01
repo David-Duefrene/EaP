6a0ee5e01f7e91d9e598cc68bc998744340e62f3
David Duefrene <1645298+David-Duefrene@users.noreply.github.com>
2022-11-30 19:02:31 -0700

    character sheet

--- APICrawler/Endpoints/Character/PublicCharacterSheet.ts
+++ APICrawler/Endpoints/Character/PublicCharacterSheet.ts
@@ -1,4 +1,4 @@
-import prisma from '../../../prisma/PrismaClient'
+import pgClient from '../../../postgres/postgresClient'
 import ESIRequest from '../../axiosRequests/ESIRequest'
 import Character from '../../../Types/APIResponses/EveOfficial/character.type'
 import CharacterAuthData from '../../../Types/APIResponses/EveOfficial/axiosTypes/characterAuthData.type'
@@ -7,25 +7,21 @@ export default (characterAuthData: CharacterAuthData) => {
 	const { characterID } = characterAuthData
 
 	return ESIRequest(`characters/${characterID}`).then(async (result: { data: Character }) => {
-		const { name } = result.data
-		const characterData = { name, characterID: BigInt(characterID) }
+		const keys = Object.keys(result.data)
+		const columns = keys.join('", "')
+		const values = keys.map((key) => result.data[key as keyof Character])
 
-		await prisma.Character.upsert({
-			where: { characterID: BigInt(characterID) },
-			update: {
-				...characterData,
-				characterSheet: {
-					update: { ...result.data },
-				},
-			},
-			create: {
-				...characterData,
-				characterSheet: {
-					create: { ...result.data },
-				},
-			},
-		}).catch((error: Error) => {
-			throw new Error('public character sheet prisma error\n', { cause: error })
+		await pgClient.query(/*SQL*/`
+			INSERT INTO public."Character" (name, "characterID", "updatedAt") VALUES ($1, $2, $3)
+			ON CONFLICT ("characterID") DO UPDATE SET name = $1, "updatedAt" = $3`,
+		[ result.data.name, characterID, new Date() ])
+
+		await pgClient.query(/*SQL*/`
+			INSERT INTO public."CharacterSheet" ("${columns}", "characterID") VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10)
+			ON CONFLICT ("characterID") DO UPDATE SET ${keys.map((key) => `"${key}" = $${keys.indexOf(key) + 1}`).join(', ')}`,
+		[ ...values, characterID ],
+		).catch((error: Error) => {
+			throw new Error('public character sheet SQL error\n', { cause: error })
 		})
 	}).catch((error: Error) => {
 		throw new Error('public character sheet API error\n', { cause: error })
