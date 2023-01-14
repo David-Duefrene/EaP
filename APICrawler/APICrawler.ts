4526e68fa2aa3ed7d21b5919d9ef2a6a70694ca1
David Duefrene <1645298+David-Duefrene@users.noreply.github.com>
2023-01-14 09:15:16 -0700

    Added try catch block

--- APICrawler/APICrawler.ts
+++ APICrawler/APICrawler.ts
@@ -17,28 +17,32 @@ const defaultReceiveMessage = (processMessages: (arg0: Log) => void) => {
 }
 
 const crawler = (sendMessage = defaultSendMessage, receiveMessage = defaultReceiveMessage) => {
-	const auth = new Auth(sendMessage, receiveMessage)
+	try {
+		const auth = new Auth(sendMessage, receiveMessage)
 
-	receiveMessage((message: Log) => {
-		if (message.type === 'refreshAPI') {
-			Object.entries(auth.characterList).forEach(async ([ characterName, characterTokens ]) => {
-				if (characterTokens.characterID < 0) {
-					return
-				}
+		receiveMessage((message: Log) => {
+			if (message.type === 'refreshAPI') {
+				Object.entries(auth.characterList).forEach(async ([ characterName, characterTokens ]) => {
+					if (characterTokens.characterID < 0) {
+						return
+					}
 
-				for (const [ key, value ] of Object.entries(endpoints.universe)) {
-					value({ ...characterTokens })
-				}
+					for (const [ key, value ] of Object.entries(endpoints.universe)) {
+						value({ ...characterTokens })
+					}
 
-				await endpoints.character.publicCharacterSheet({ ...characterTokens })
+					await endpoints.character.publicCharacterSheet({ ...characterTokens })
 
-				for (const [ key, value ] of Object.entries(endpoints.character)) {
-					if (key === 'publicCharacterData') continue
-					value({ ...characterTokens })
-				}
-			})
-		}
-	})
+					for (const [ key, value ] of Object.entries(endpoints.character)) {
+						if (key === 'publicCharacterData') continue
+						value({ ...characterTokens })
+					}
+				})
+			}
+		})
+	} catch (error) {
+		receiveMessage(error)
+	}
 }
 
 export default crawler
