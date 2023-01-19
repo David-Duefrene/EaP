63610d109cf818d75707f82f095630676361a59d
David Duefrene <1645298+David-Duefrene@users.noreply.github.com>
2023-01-19 11:48:57 -0700

    navigation json added, character nav bar moved to i18n

--- React/i18n.ts
+++ React/i18n.ts
@@ -3,11 +3,12 @@ import { initReactI18next } from 'react-i18next'
 
 import keys from './public/locales/en/keys.json'
 import enums from './public/locales/en/enums.json'
+import navigation from './public/locales/en/navigation.json'
 
 i18n.use(initReactI18next).init({
 	lng: 'en',
 	resources: {
-		en: { keys, enums },
+		en: { keys, enums, navigation },
 	},
 	interpolation: {
 		escapeValue: false,
