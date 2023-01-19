eff7cf9961f026a940d1d34985f374dca0aedcb1
David Duefrene <1645298+David-Duefrene@users.noreply.github.com>
2023-01-19 12:46:51 -0700

    now loads from file structure

--- React/i18n.ts
+++ React/i18n.ts
@@ -1,16 +1,23 @@
 import i18n from 'i18next'
 import { initReactI18next } from 'react-i18next'
 
-import keys from './public/locales/en/keys.json'
-import enums from './public/locales/en/enums.json'
-import navigation from './public/locales/en/navigation.json'
-import buttons from './public/locales/en/buttons.json'
+const endpoints = import.meta.globEager('./public/locales/**/*.json')
+const endpointObject: Record<string, Record<string, () => Promise<never>>> = {}
+
+for (const [ key, value ] of Object.entries(endpoints)) {
+	const directory = key.slice(2, -5).split('/').map((item) => `${item[0].toLowerCase()}${item.slice(1)}`)
+
+	if (directory[2] in endpointObject) {
+		endpointObject[directory[2]][directory[3]] = value.default
+	} else {
+		endpointObject[directory[2]] = {}
+		endpointObject[directory[2]][directory[3]] = value.default
+	}
+}
 
 i18n.use(initReactI18next).init({
 	lng: 'en',
-	resources: {
-		en: { keys, enums, navigation, buttons },
-	},
+	resources: endpointObject,
 	interpolation: {
 		escapeValue: false,
 	},
