ae07d594d692c9badc79f8e33ac99dc59bd87bac
David Duefrene <1645298+David-Duefrene@users.noreply.github.com>
2023-01-23 17:55:47 -0700

    fixed test

--- APICrawler/Endpoints/Clones/Clones/Clones.test.ts
+++ APICrawler/Endpoints/Clones/Clones/Clones.test.ts
@@ -14,16 +14,24 @@ describe('clones', () => {
 	test('should be able to get clones', async () => {
 		vi.mock('../../../axiosRequests/ESIRequest', () => ({
 			default: vi.fn().mockResolvedValue({
-				data: [
-					{
-						characterID: BigInt(1),
-						jumpCloneID: 1,
-						locationID: BigInt(1),
-						locationType: 'station',
-						implants: [ 1, 2 ],
-						name: 'Clone',
+				data: {
+					characterID: BigInt(1),
+					jump_clones: [
+						{
+							implants: [ 1, 2 ],
+							jump_clone_id: 1,
+							location_id: BigInt(1),
+							location_type: 'station',
+							name: 'Clone',
+						}
+					],
+					home_location: {
+						location_id: BigInt(1),
+						location_type: 'station',
 					},
-				],
+					last_clone_jump_date: '2021-01-01T00:00:00Z',
+					last_station_change_date: '2021-01-01T00:00:00Z',
+				},
 			}),
 		}))
 
@@ -33,17 +41,17 @@ describe('clones', () => {
 
 		await clones({ characterID: BigInt(1), accessToken: 'Token' })
 
-		expect(ESIRequest).toBeCalledTimes(1)
-		expect(ESIRequest).toBeCalledWith('/characters/1/clones/', 'Token')
-		expect(pgUpsert).toBeCalledTimes(1)
-		const mockData = {
+		expect(ESIRequest).toBeCalledTimes(2)
+		expect(ESIRequest).toBeCalledWith('characters/1/clones/', 'Token')
+		expect(pgUpsert).toBeCalledTimes(2)
+		const mockData ={
 			characterID: BigInt(1),
-			jumpCloneID: 1,
-			locationID: BigInt(1),
-			locationType: 'station',
 			implants: [ 1, 2 ],
+			jump_clone_id: 1,
+			location_id: BigInt(1),
+			location_type: 'station',
 			name: 'Clone',
 		}
-		expect(pgUpsert).toBeCalledWith('clone', mockData, [ 'character_id', 'jump_clone_id' ])
+		expect(pgUpsert).toBeCalledWith('clones', mockData, [ 'character_id', 'jump_clone_id' ])
 	})
 })
